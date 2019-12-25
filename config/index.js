1b61b2917c254e2e076db252170d170f124d96a7
yogain19 <yogendrasaxena56@gmail.com>
2019-12-25 12:20:51 +0530

    fix

---
+++ config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: process.env.oauth_consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token_key: process.env.access_token_key,
+    access_token_secret: process.env.access_token_secret
+  }
--- services/index.js
+++ services/index.js
@@ -1,19 +1,26 @@
 const request = require("request");
+const twi = require("twitter");
+const config = require('../config');
+var T = new twi(config);
+
+// Set up your search parameters
+var params = {
+  q: '#nodejs',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en',
+}
 
 exports.callTwitterApi = function(screen_name) {
+  params.screen_name = screen_name;
   return new Promise(resolve => {
-    var options = {
-      method: "GET",
-      url: `https://api.twitter.com/1.1/friends/list.json?screen_name=${screen_name}`,
-      headers: {
-        Authorization: `OAuth oauth_consumer_key=${process.env.oauth_consumer_key},oauth_token=${process.env.oauth_token},oauth_signature_method=${process.env.oauth_signature_method},oauth_timestamp=${process.env.oauth_timestamp},oauth_nonce=${process.env.oauth_nonce},oauth_version=${process.env.oauth_version},oauth_signature=${process.env.oauth_signature}`
-      },
-      json: true
-    };
-    request(options, function(error, response) {
-      if (error) return resolve({ serverStatus: false });
-      return resolve({ serverStatus: true, data: response.body.users });
-    });
+    T.get('friends/list.json', params, function(err, data, response) {
+      if(!err){
+        return resolve({ serverStatus: true, data: data.users });
+      } else {
+        resolve({ serverStatus: false });
+      }
+    })
   });
 };
 
