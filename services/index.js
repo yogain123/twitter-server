33656a9e1dd1893ce263536f08da0a1dfa05652f
yogain19 <yogendrasaxena56@gmail.com>
2019-12-25 13:34:43 +0530

    next cursor pagination fix

--- services/index.js
+++ services/index.js
@@ -10,18 +10,38 @@ var params = {
   lang: 'en',
 }
 
-exports.callTwitterApi = function(screen_name) {
+exports.callTwitterApi = async function(screen_name) {
+  let resultArray = [];
+  let cursor = undefined;
+  let getDataFromAPI;
+  do{
+    getDataFromAPI = await promiseTwitterData(screen_name,cursor);
+    if(!getDataFromAPI.serverStatus){
+      return { serverStatus: false }
+    }
+    else{
+      resultArray = [...resultArray, ...getDataFromAPI.data];
+      cursor = getDataFromAPI.cursor;
+    }
+  }
+  while(getDataFromAPI.cursor!==0);
+
+  return resultArray;
+};
+
+const promiseTwitterData = function(screen_name, cursor){
   params.screen_name = screen_name;
+  cursor===undefined?"":params.cursor=cursor;
   return new Promise(resolve => {
     T.get('friends/list.json', params, function(err, data, response) {
       if(!err){
-        return resolve({ serverStatus: true, data: data.users });
+        return resolve({ serverStatus: true, data: data.users , cursor:data.next_cursor});
       } else {
         resolve({ serverStatus: false });
       }
     })
   });
-};
+}
 
 exports.getMutualFriend = function(user1list, user2list) {
   user1list = user1list.map(item => item.screen_name);
